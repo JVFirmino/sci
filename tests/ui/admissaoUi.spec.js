bdc4552e5f502df5d86f5b3121ceac1750715c1d
JVFirmino <joaov3865@gmail.com>
2025-10-10 00:21:31 -0300

    teste

--- tests/ui/admissaoUi.spec.js
+++ tests/ui/admissaoUi.spec.js
@@ -1,180 +1,180 @@
-// import { test, expect } from "@playwright/test";
-// import { LoginPage } from "../../pages/loginPage";
-// import { AdmissaoPage } from "../../pages/admissaoPage";
-// import { ROTAS } from "../../fixture/rotaFixture";
-// import { UiActionsHelpers } from "../../src/helpers/uiActionsHelpers";
-// import { USUARIOS } from "../../fixture/usuarioFixture";
-// import { UsuarioHelpers }  from "../../src/helpers/usuarioHelpers";
-// import { MENSAGENS } from "../../fixture/mensagemFixture";
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../../pages/loginPage";
+import { AdmissaoPage } from "../../pages/admissaoPage";
+import { ROTAS } from "../../fixture/rotaFixture";
+import { UiActionsHelpers } from "../../src/helpers/uiActionsHelpers";
+import { USUARIOS } from "../../fixture/usuarioFixture";
+import { UsuarioHelpers }  from "../../src/helpers/usuarioHelpers";
+import { MENSAGENS } from "../../fixture/mensagemFixture";
 
-// test.describe("Admissão Preliminar", { tag: ["@ADMISSAO_PRELIMINAR"] }, () => {
-//     let uiActionsHelpers;
-//     let loginPage;
-//     let admissaoPage;
-//     let usuarioHelpers;
+test.describe("Admissão Preliminar", { tag: ["@ADMISSAO_PRELIMINAR"] }, () => {
+    let uiActionsHelpers;
+    let loginPage;
+    let admissaoPage;
+    let usuarioHelpers;
 
-//     test.beforeEach(async ({ page }) => {
-//         console.log(`Iniciando o teste: ${test.info().title}`);
+    test.beforeEach(async ({ page }) => {
+        console.log(`Iniciando o teste: ${test.info().title}`);
 
-//         uiActionsHelpers = new UiActionsHelpers(page);
-//         loginPage = new LoginPage(page);
-//         admissaoPage = new AdmissaoPage(page);
-//         usuarioHelpers = new UsuarioHelpers();
+        uiActionsHelpers = new UiActionsHelpers(page);
+        loginPage = new LoginPage(page);
+        admissaoPage = new AdmissaoPage(page);
+        usuarioHelpers = new UsuarioHelpers();
 
-//         // Acessar a página de login e verificar o título
-//         await uiActionsHelpers.navegarEVerificarTituloPagina(ROTAS.login.url, ROTAS.login.titulo);
-//     });
+        // Acessar a página de login e verificar o título
+        await uiActionsHelpers.navegarEVerificarTituloPagina(ROTAS.login.url, ROTAS.login.titulo);
+    });
 
-//     test.afterEach(async ({ page }) => {
-//         console.log(`Finalizando o teste: ${test.info().title}`);
-//     });
+    test.afterEach(async ({ page }) => {
+        console.log(`Finalizando o teste: ${test.info().title}`);
+    });
 
-//     /*
-//     Cenário 1: Realizar admissão preliminar com sucesso - Contribuinte Dirigente sindical
+    /*
+    Cenário 1: Realizar admissão preliminar com sucesso - Contribuinte Dirigente sindical
 
-//     Passo a Passo:
-//     1. Gerar os dados de um novo usuário.
-//     2. Realizar login com um usuário válido.
-//     3. Acessar o módulo RHNetSocial.
-//     4. Realizar o processo de admissão preliminar com o tipo "Contribuinte" e categoria "Dirigente sindical".
-//     5. Verificar se a mensagem de sucesso da admissão é exibida na tela.
+    Passo a Passo:
+    1. Gerar os dados de um novo usuário.
+    2. Realizar login com um usuário válido.
+    3. Acessar o módulo RHNetSocial.
+    4. Realizar o processo de admissão preliminar com o tipo "Contribuinte" e categoria "Dirigente sindical".
+    5. Verificar se a mensagem de sucesso da admissão é exibida na tela.
 
-//     Resultado Esperado:
-//     - O sistema deve concluir o processo de admissão preliminar com sucesso e exibir a mensagem definida em `MENSAGENS.admissao.sucessoAdmissao`.
+    Resultado Esperado:
+    - O sistema deve concluir o processo de admissão preliminar com sucesso e exibir a mensagem definida em `MENSAGENS.admissao.sucessoAdmissao`.
 
-//     Motivo:
-//     - Este teste garante que o processo de admissão preliminar funciona corretamente para o tipo de vínculo "Contribuinte" com a categoria "Dirigente sindical".
+    Motivo:
+    - Este teste garante que o processo de admissão preliminar funciona corretamente para o tipo de vínculo "Contribuinte" com a categoria "Dirigente sindical".
 
-//     Criticidade:
-//     - Alta. A admissão preliminar é uma etapa essencial no cadastro de vínculos no sistema e deve funcionar corretamente para todos os tipos e categorias.
-//     */
-//     test("realizar admissão preliminar com sucesso contribuinte dirigente sindical", { tag: "@ADMISSAO_PRELIMINAR_SUCESSO" }, async({ page }) => {
-//         // Gerar um usuário
-//         const usuario = await usuarioHelpers.gerarUsuario();
+    Criticidade:
+    - Alta. A admissão preliminar é uma etapa essencial no cadastro de vínculos no sistema e deve funcionar corretamente para todos os tipos e categorias.
+    */
+    test("realizar admissão preliminar com sucesso contribuinte dirigente sindical", { tag: "@ADMISSAO_PRELIMINAR_SUCESSO" }, async({ page }) => {
+        // Gerar um usuário
+        const usuario = await usuarioHelpers.gerarUsuario();
 
-//         // Realizar login
-//         await loginPage.realizarLogin(USUARIOS.valido);
+        // Realizar login
+        await loginPage.realizarLogin(USUARIOS.valido);
 
-//         // Acessar o módulo RHNetSocial
-//         await uiActionsHelpers.acessarModuloRHNetSocial("rhnetsocial");
+        // Acessar o módulo RHNetSocial
+        await uiActionsHelpers.acessarModuloRHNetSocial("rhnetsocial");
 
-//         // Realizar admissão preliminar
-//         await admissaoPage.realizarAdmissaoPreliminar(usuario, "Contribuinte", "Dirigente sindical");
+        // Realizar admissão preliminar
+        await admissaoPage.realizarAdmissaoPreliminar(usuario, "Contribuinte", "Dirigente sindical");
 
-//         // Verificar se a admissão foi realizada com sucesso
-//         await page.waitForTimeout(3000);
-//         await expect(page.getByText(MENSAGENS.admissao.sucessoAdmissao)).toBeVisible();
-//     });
+        // Verificar se a admissão foi realizada com sucesso
+        await page.waitForSelector(`text=${MENSAGENS.admissao.sucessoAdmissao}`, { timeout: 15000 });
+        await expect(page.getByText(MENSAGENS.admissao.sucessoAdmissao)).toBeVisible();
+    });
 
-//     /*
-//     Cenário 2: Realizar admissão preliminar com sucesso - Contribuinte Pró-Laborista
+    /*
+    Cenário 2: Realizar admissão preliminar com sucesso - Contribuinte Pró-Laborista
 
-//     Passo a Passo:
-//     1. Gerar os dados de um novo usuário.
-//     2. Realizar login com um usuário válido.
-//     3. Acessar o módulo RHNetSocial.
-//     4. Realizar o processo de admissão preliminar com o tipo "Contribuinte" e categoria "Pró-Laborista".
-//     5. Verificar se a mensagem de sucesso da admissão é exibida na tela.
+    Passo a Passo:
+    1. Gerar os dados de um novo usuário.
+    2. Realizar login com um usuário válido.
+    3. Acessar o módulo RHNetSocial.
+    4. Realizar o processo de admissão preliminar com o tipo "Contribuinte" e categoria "Pró-Laborista".
+    5. Verificar se a mensagem de sucesso da admissão é exibida na tela.
 
-//     Resultado Esperado:
-//     - O sistema deve concluir o processo de admissão preliminar com sucesso e exibir a mensagem definida em `MENSAGENS.admissao.sucessoAdmissao`.
+    Resultado Esperado:
+    - O sistema deve concluir o processo de admissão preliminar com sucesso e exibir a mensagem definida em `MENSAGENS.admissao.sucessoAdmissao`.
 
-//     Motivo:
-//     - Este teste garante que o processo de admissão preliminar funciona corretamente para o tipo de vínculo "Contribuinte" com a categoria "Pró-Laborista".
+    Motivo:
+    - Este teste garante que o processo de admissão preliminar funciona corretamente para o tipo de vínculo "Contribuinte" com a categoria "Pró-Laborista".
 
-//     Criticidade:
-//     - Alta. A admissão preliminar é uma etapa essencial no cadastro de vínculos no sistema e deve funcionar corretamente para todos os tipos e categorias.
-//     */
-//     test("realizar admissão preliminar com sucesso contribuinte pró-laborista", { tag: "@ADMISSAO_PRELIMINAR_SUCESSO" }, async({ page }) => {
-//         // Gerar um usuário
-//         const usuario = await usuarioHelpers.gerarUsuario();
+    Criticidade:
+    - Alta. A admissão preliminar é uma etapa essencial no cadastro de vínculos no sistema e deve funcionar corretamente para todos os tipos e categorias.
+    */
+    test("realizar admissão preliminar com sucesso contribuinte pró-laborista", { tag: "@ADMISSAO_PRELIMINAR_SUCESSO" }, async({ page }) => {
+        // Gerar um usuário
+        const usuario = await usuarioHelpers.gerarUsuario();
 
-//         // Realizar login
-//         await loginPage.realizarLogin(USUARIOS.valido);
+        // Realizar login
+        await loginPage.realizarLogin(USUARIOS.valido);
 
-//         // Acessar o módulo RHNetSocial
-//         await uiActionsHelpers.acessarModuloRHNetSocial("rhnetsocial");
+        // Acessar o módulo RHNetSocial
+        await uiActionsHelpers.acessarModuloRHNetSocial("rhnetsocial");
 
-//         // Realizar admissão preliminar
-//         await admissaoPage.realizarAdmissaoPreliminar(usuario, "Contribuinte", "Pró-Laborista");
+        // Realizar admissão preliminar
+        await admissaoPage.realizarAdmissaoPreliminar(usuario, "Contribuinte", "Pró-Laborista");
 
-//         // Verificar se a admissão foi realizada com sucesso
-//         await page.waitForTimeout(3000);
-//         await expect(page.getByText(MENSAGENS.admissao.sucessoAdmissao)).toBeVisible();
-//     });
+        // Verificar se a admissão foi realizada com sucesso
+        await page.waitForSelector(`text=${MENSAGENS.admissao.sucessoAdmissao}`, { timeout: 15000 });
+        await expect(page.getByText(MENSAGENS.admissao.sucessoAdmissao)).toBeVisible();
+    });
 
-//     /*
-//     Cenário 3: Realizar admissão preliminar com sucesso - Contribuinte Servidor Público
+    /*
+    Cenário 3: Realizar admissão preliminar com sucesso - Contribuinte Servidor Público
 
-//     Passo a Passo:
-//     1. Gerar os dados de um novo usuário.
-//     2. Realizar login com um usuário válido.
-//     3. Acessar o módulo RHNetSocial.
-//     4. Realizar o processo de admissão preliminar com o tipo "Contribuinte" e categoria "Servidor Público".
-//     5. Verificar se a mensagem de sucesso da admissão é exibida na tela.
+    Passo a Passo:
+    1. Gerar os dados de um novo usuário.
+    2. Realizar login com um usuário válido.
+    3. Acessar o módulo RHNetSocial.
+    4. Realizar o processo de admissão preliminar com o tipo "Contribuinte" e categoria "Servidor Público".
+    5. Verificar se a mensagem de sucesso da admissão é exibida na tela.
 
-//     Resultado Esperado:
-//     - O sistema deve concluir o processo de admissão preliminar com sucesso e exibir a mensagem definida em `MENSAGENS.admissao.sucessoAdmissao`.
+    Resultado Esperado:
+    - O sistema deve concluir o processo de admissão preliminar com sucesso e exibir a mensagem definida em `MENSAGENS.admissao.sucessoAdmissao`.
 
-//     Motivo:
-//     - Este teste garante que o processo de admissão preliminar funciona corretamente para o tipo de vínculo "Contribuinte" com a categoria "Servidor Público".
+    Motivo:
+    - Este teste garante que o processo de admissão preliminar funciona corretamente para o tipo de vínculo "Contribuinte" com a categoria "Servidor Público".
 
-//     Criticidade:
-//     - Alta. A admissão preliminar é uma etapa essencial no cadastro de vínculos no sistema e deve funcionar corretamente para todos os tipos e categorias.
-//     */
-//     test("realizar admissão preliminar com sucesso contribuinte servidor público", { tag: "@ADMISSAO_PRELIMINAR_SUCESSO" }, async({ page }) => {
-//         // Gerar um usuário
-//         const usuario = await usuarioHelpers.gerarUsuario();
+    Criticidade:
+    - Alta. A admissão preliminar é uma etapa essencial no cadastro de vínculos no sistema e deve funcionar corretamente para todos os tipos e categorias.
+    */
+    test("realizar admissão preliminar com sucesso contribuinte servidor público", { tag: "@ADMISSAO_PRELIMINAR_SUCESSO" }, async({ page }) => {
+        // Gerar um usuário
+        const usuario = await usuarioHelpers.gerarUsuario();
 
-//         // Realizar login
-//         await loginPage.realizarLogin(USUARIOS.valido);
+        // Realizar login
+        await loginPage.realizarLogin(USUARIOS.valido);
 
-//         // Acessar o módulo RHNetSocial
-//         await uiActionsHelpers.acessarModuloRHNetSocial("rhnetsocial");
+        // Acessar o módulo RHNetSocial
+        await uiActionsHelpers.acessarModuloRHNetSocial("rhnetsocial");
 
-//         // Realizar admissão preliminar
-//         await admissaoPage.realizarAdmissaoPreliminar(usuario, "Contribuinte", "Servidor Público");
+        // Realizar admissão preliminar
+        await admissaoPage.realizarAdmissaoPreliminar(usuario, "Contribuinte", "Servidor Público");
 
-//         // Verificar se a admissão foi realizada com sucesso
-//         await page.waitForTimeout(3000);
-//         await expect(page.getByText(MENSAGENS.admissao.sucessoAdmissao)).toBeVisible();
-//     });
+        // Verificar se a admissão foi realizada com sucesso
+        await page.waitForSelector(`text=${MENSAGENS.admissao.sucessoAdmissao}`, { timeout: 15000 });
+        await expect(page.getByText(MENSAGENS.admissao.sucessoAdmissao)).toBeVisible();
+    });
 
 
-//     /*
-//     Cenário 4: Realizar admissão preliminar com sucesso - Contribuinte Sócio cotista
+    /*
+    Cenário 4: Realizar admissão preliminar com sucesso - Contribuinte Sócio cotista
 
-//     Passo a Passo:
-//     1. Gerar os dados de um novo usuário.
-//     2. Realizar login com um usuário válido.
-//     3. Acessar o módulo RHNetSocial.
-//     4. Realizar o processo de admissão preliminar com o tipo "Contribuinte" e categoria "Sócio cotista".
-//     5. Verificar se a mensagem de sucesso da admissão é exibida na tela.
+    Passo a Passo:
+    1. Gerar os dados de um novo usuário.
+    2. Realizar login com um usuário válido.
+    3. Acessar o módulo RHNetSocial.
+    4. Realizar o processo de admissão preliminar com o tipo "Contribuinte" e categoria "Sócio cotista".
+    5. Verificar se a mensagem de sucesso da admissão é exibida na tela.
 
-//     Resultado Esperado:
-//     - O sistema deve concluir o processo de admissão preliminar com sucesso e exibir a mensagem definida em `MENSAGENS.admissao.sucessoAdmissao`.
+    Resultado Esperado:
+    - O sistema deve concluir o processo de admissão preliminar com sucesso e exibir a mensagem definida em `MENSAGENS.admissao.sucessoAdmissao`.
 
-//     Motivo:
-//     - Este teste garante que o processo de admissão preliminar funciona corretamente para o tipo de vínculo "Contribuinte" com a categoria "Sócio cotista".
+    Motivo:
+    - Este teste garante que o processo de admissão preliminar funciona corretamente para o tipo de vínculo "Contribuinte" com a categoria "Sócio cotista".
 
-//     Criticidade:
-//     - Alta. A admissão preliminar é uma etapa essencial no cadastro de vínculos no sistema e deve funcionar corretamente para todos os tipos e categorias.
-//     */
-//     test("realizar admissão preliminar com sucesso contribuinte sócio cotista", { tag: "@ADMISSAO_PRELIMINAR_SUCESSO" }, async({ page }) => {
-//         // Gerar um usuário
-//         const usuario = await usuarioHelpers.gerarUsuario();
+    Criticidade:
+    - Alta. A admissão preliminar é uma etapa essencial no cadastro de vínculos no sistema e deve funcionar corretamente para todos os tipos e categorias.
+    */
+    test("realizar admissão preliminar com sucesso contribuinte sócio cotista", { tag: "@ADMISSAO_PRELIMINAR_SUCESSO" }, async({ page }) => {
+        // Gerar um usuário
+        const usuario = await usuarioHelpers.gerarUsuario();
 
-//         // Realizar login
-//         await loginPage.realizarLogin(USUARIOS.valido);
+        // Realizar login
+        await loginPage.realizarLogin(USUARIOS.valido);
 
-//         // Acessar o módulo RHNetSocial
-//         await uiActionsHelpers.acessarModuloRHNetSocial("rhnetsocial");
+        // Acessar o módulo RHNetSocial
+        await uiActionsHelpers.acessarModuloRHNetSocial("rhnetsocial");
 
-//         // Realizar admissão preliminar
-//         await admissaoPage.realizarAdmissaoPreliminar(usuario, "Contribuinte", "Sócio cotista");
+        // Realizar admissão preliminar
+        await admissaoPage.realizarAdmissaoPreliminar(usuario, "Contribuinte", "Sócio cotista");
 
-//         // Verificar se a admissão foi realizada com sucesso
-//         await page.waitForTimeout(3000);
-//         await expect(page.getByText(MENSAGENS.admissao.sucessoAdmissao)).toBeVisible();
-//     });
-// });
+        // Verificar se a admissão foi realizada com sucesso
+        await page.waitForSelector(`text=${MENSAGENS.admissao.sucessoAdmissao}`, { timeout: 15000 });
+        await expect(page.getByText(MENSAGENS.admissao.sucessoAdmissao)).toBeVisible();
+    });
+});
